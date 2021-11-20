b409d8910ba2169cb92c63e2c3183f10fca48315
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2021-11-21 00:26:46 +0530

    install fbsdk-next and create signin with facebook

--- app/navigation/AuthProvider.js
+++ app/navigation/AuthProvider.js
@@ -1,6 +1,7 @@
 import React, {createContext, useState} from 'react';
 import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {LoginManager, AccessToken} from 'react-native-fbsdk-next';
 
 export const AuthContext = createContext();
 
@@ -25,6 +26,35 @@ export const AuthProvider = ({children}) => {
             const googleCredential =
               auth.GoogleAuthProvider.credential(idToken);
             await auth().signInWithCredential(googleCredential);
+          } catch (e) {
+            console.log({e});
+          }
+        },
+        fbLogin: async () => {
+          try {
+            const result = await LoginManager.logInWithPermissions([
+              'public_profile',
+              'email',
+            ]);
+
+            if (result.isCancelled) {
+              throw 'User cancelled the login process';
+            }
+
+            // Once signed in, get the users AccesToken
+            const data = await AccessToken.getCurrentAccessToken();
+
+            if (!data) {
+              throw 'Something went wrong obtaining access token';
+            }
+
+            // Create a Firebase credential with the AccessToken
+            const facebookCredential = auth.FacebookAuthProvider.credential(
+              data.accessToken,
+            );
+
+            // Sign-in the user with the credential
+            await auth().signInWithCredential(facebookCredential);
           } catch (e) {
             console.log({e});
           }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -17,7 +17,7 @@ const LoginScreen = ({navigation}) => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-  const {login, googleLogin} = useContext(AuthContext);
+  const {login, googleLogin, fbLogin} = useContext(AuthContext);
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -59,7 +59,7 @@ const LoginScreen = ({navigation}) => {
             btnType="facebook"
             color="#4867aa"
             backgroundColor="#e6eaf4"
-            onPress={() => {}}
+            onPress={() => fbLogin()}
           />
 
           <SocialButton
