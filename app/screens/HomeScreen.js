aff396fa38f3d630879cca7ec4ed1304129ae914
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2021-11-20 13:11:00 +0530

    install firebase and authenticate

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,8 @@
+import React, {useContext, useState} from 'react';
+import {View} from 'react-native';
+
+const HomeScreen = ({navigation}) => {
+  return <View>Welcome</View>;
+};
+
+export default HomeScreen;
